2e8cb5d5932df74351d797d9a6f241be4be6bc76
Raghavendra <RaghavendraJ@ziniosedge.com>
2025-05-16 21:44:14 +0530

    backend code change

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -47,13 +47,16 @@ const ContactForm = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://combat24backend-1.onrender.com/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (response.ok) {
         alert("Email sent successfully!");
