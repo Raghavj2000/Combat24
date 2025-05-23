62e5a8a1774aae10b31ace3dcba2c3ec9b883ce1
Raghavendra <raghavjayateerth@gmail.com>
2025-05-24 00:57:32 +0530

    removed strictmode

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ import "swiper/css/pagination";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 reportWebVitals();
